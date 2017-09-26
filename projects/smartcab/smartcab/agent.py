8c4f48b46e9c116e5dab3239fda75a79f30eb1c3
jeongwhanchoi <jeongwhan.choi@icloud.com>
2017-09-27 08:22:36 +0900

    set 'update_delay' to a small value

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -174,7 +174,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, update_delay=0.01)
     
     ##############
     # Run the simulator
