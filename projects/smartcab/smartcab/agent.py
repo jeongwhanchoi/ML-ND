604f9a0c846d36f6d78d420a589b17d7bc28721b
jeongwhanchoi <jeongwhan.choi@icloud.com>
2017-09-29 12:33:37 +0900

    edit typo

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -62,7 +62,7 @@ class LearningAgent(Agent):
         # With the hand-engineered features, this learning process gets entirely negated.
         
         # Set 'state' as a tuple of relevant data for the agent
-        state = waypoint, inputs['lights'], inputs['left'], inputs['oncoming']
+        state = waypoint, inputs['light'], inputs['left'], inputs['oncoming']
         
         return state
 
